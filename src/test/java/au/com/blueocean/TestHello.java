b6c91b4a0745520c025ae481155651374315328e
Anita Park <anita@smpark.com.au>
2017-05-14 10:53:52 +0800

    Added tests

---
+++ src/test/java/au/com/blueocean/TestHello.java
@@ -0,0 +1,14 @@
+package au.com.blueocean;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestHello
+{
+	@Test
+	public void test1()
+	{
+		assertEquals(1, 1);
+	}
+}
